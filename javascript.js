7aa813dd9ab58355268786744cf6f55b58e1ef03
NexusHiveMind <rodrigo.daniel.pena@gmail.com>
2024-05-04 15:56:39 -0300

    Create user variables, apply use strict mode

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+"use strict";
+
+let new_user_name;
+let new_user_password;
+let current_user_name;
+let current_user_password;
+
+current_user_name = "default"
+// user_name = prompt("What is your username?");
+console.log(current_user_name)
