57926bac45feb7851ea55cb80db8139dea05a620
NexusHiveMind <rodrigo.daniel.pena@gmail.com>
2024-05-05 15:33:45 -0300

    Add commented reference for dropdown menu

--- javascript.js
+++ javascript.js
@@ -9,7 +9,7 @@ current_user_name = "default"
 // user_name = prompt("What is your username?");
 console.log(current_user_name)
 
-/*
+/* Buttons
 HTML
 <button>Press me</button>
 <div id="greeting"></div>
@@ -22,4 +22,46 @@ function greet() {
   greeting.textContent = `Hello ${name}, nice to see you!`;
 }
 button.addEventListener("click", greet);
+*/
+
+/* Select
+HTML
+<label for="weather">Select the weather type today: </label>
+<select id="weather">
+  <option value="">--Make a choice--</option>
+  <option value="sunny">Sunny</option>
+  <option value="rainy">Rainy</option>
+  <option value="snowing">Snowing</option>
+  <option value="overcast">Overcast</option>
+</select>
+<p></p>
+
+Javascript
+const select = document.querySelector("select");
+const para = document.querySelector("p");
+
+select.addEventListener("change", setWeather);
+
+function setWeather() {
+  const choice = select.value;
+
+  if (choice === "sunny") {
+    para.textContent =
+      "It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.";
+  } else if (choice === "rainy") {
+    para.textContent =
+      "Rain is falling outside; take a rain coat and an umbrella, and don't stay out for too long.";
+  } else if (choice === "snowing") {
+    para.textContent =
+      "The snow is coming down — it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.";
+  } else if (choice === "overcast") {
+    para.textContent =
+      "It isn't raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.";
+  } else {
+    para.textContent = "";
+  }
+}
+
+
+
 */
