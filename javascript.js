4811b803b0074904c82493f3f540e680ddab59f8
NexusHiveMind <rodrigo.daniel.pena@gmail.com>
2024-05-31 23:36:21 -0300

    Add dinamically new characters to DOM in review_characters

--- javascript.js
+++ javascript.js
@@ -18,3 +18,33 @@ function register (user,password){
 }
 
 /* Create a function that validates the login form with the aid of the e-mail check function */
+
+// Active learning: A dynamic list -> Find out how to say "only active in review_characters.html"
+let unorderedList = document.querySelector(".character_list");
+let inputWithData = document.querySelector("#item");
+let addItemButton = document.querySelector("#add_item");
+let value = '';
+
+addItemButton.addEventListener("click",addItemWithDeleteButtonToList);
+
+function addItemWithDeleteButtonToList () {
+        value = inputWithData.value;
+        inputWithData.value = '';
+        if (value !== ""){
+          const listItem = document.createElement("li");
+          const spanItem = document.createElement("span");
+          const buttonItem = document.createElement("button");
+          
+          spanItem.textContent = value;
+          buttonItem.textContent = "Delete";
+          listItem.appendChild(spanItem);
+          listItem.appendChild(buttonItem);
+          unorderedList.appendChild(listItem);
+
+          buttonItem.addEventListener("click",()=>{
+                  unorderedList.removeChild(listItem);
+                  inputWithData.focus();
+          });
+        };
+        inputWithData.focus();
+}
